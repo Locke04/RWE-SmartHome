7d3adcd6013ea4dd887b9b2dfca3e13ccf522979
@Locke_04 <renebuehring@Macintosh.local>
2014-01-13 19:24:56 +0100

    as

--- lib/siriproxy-smarthome.rb
+++ lib/siriproxy-smarthome.rb
@@ -3,6 +3,7 @@ require 'cora'
 require 'siri_objects'
 require 'pp'
 require 'open-uri'
+require 'net/http'
 
 class SiriProxy::Plugin::SmartHome < SiriProxy::Plugin
     def initialize(config)
@@ -104,7 +105,8 @@ end
 
 #Deaktivieren
 listen_for /Küche aus/i do
-    uri = "https://mobile.rwe-smarthome.de/MobileWeb/JsonApi/SetActuatorValue/?Id=9fc52200-1747-408a-a82f-8ee294511b70&Value=0"
+    Net::HTTP.get('https://mobile.rwe-smarthome.de/MobileWeb/JsonApi/SetActuatorValue/?Id=9fc52200-1747-408a-a82f-8ee294511b70&Value=0', '')
+    #uri = "https://mobile.rwe-smarthome.de/MobileWeb/JsonApi/SetActuatorValue/?Id=9fc52200-1747-408a-a82f-8ee294511b70&Value=0"
     say "Küche wird ausgeschaltet"
     request_completed
 end
