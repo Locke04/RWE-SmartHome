826710f96765b183a8964dd35eb7d140ab29d33f
@Locke_04 <renebuehring@Macintosh.local>
2014-01-15 20:42:07 +0100

    1.6.1

--- lib/siriproxy-smarthome.rb
+++ lib/siriproxy-smarthome.rb
@@ -38,10 +38,11 @@ add_property_to_class(OpenLink, :ref)
 
 listen_for /(Küche einschalten|Küche an|Küchenlicht an|Küchenlicht einschalten)/i do
     say "Küchenlicht wird eingeschaltet"
+    sleep(1)
     system"open -a '/Applications/Google Chrome.app' 'https://mobile.rwe-smarthome.de/MobileWeb/JsonApi/SetActuatorValue/?Id=9fc52200-1747-408a-a82f-8ee294511b70&Value=1'"
     sleep(5)
     system"killall 'Google Chrome'"
-    sleep(1)
+    sleep(5)
     system"open -a '/Applications/Google Chrome.app' 'https://mobile.rwe-smarthome.de/MobileWeb/OverviewAndControl'"
     request_completed
 end
