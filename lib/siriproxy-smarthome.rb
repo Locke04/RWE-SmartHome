5cdc50c47e1087c4005838e3ef3c726b77cf1a06
@Locke_04 <renebuehring@Macintosh.local>
2014-01-15 22:48:44 +0100

    sad

--- lib/siriproxy-smarthome.rb
+++ lib/siriproxy-smarthome.rb
@@ -2,6 +2,8 @@
 require 'cora'
 require 'siri_objects'
 require 'pp'
+require 'net/http'
+require 'uri'
 
 class SiriProxy::Plugin::SmartHome < SiriProxy::Plugin
     def initialize(config)
@@ -19,7 +21,7 @@ class SiriProxy::Plugin::SmartHome < SiriProxy::Plugin
 
 listen_for /(Küche einschalten|Küche an|Küchenlicht an|Küchenlicht einschalten)/i do
 say "Küche"
-Net::HTTP.get(URI.parse("https://mobile.rwe-smarthome.de/MobileWeb/JsonApi/SetActuatorValue/?Id=9fc52200-1747-408a-a82f-8ee294511b70&Value=1"))
+response = Net::HTTP.get(URI.parse("https://mobile.rwe-smarthome.de/MobileWeb/JsonApi/SetActuatorValue/?Id=9fc52200-1747-408a-a82f-8ee294511b70&Value=1"))
 say "An"
 request_completed
 end
